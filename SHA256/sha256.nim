3cb994c8c219ef0a3eefa5e1b9acc4bb0472feac
hamidb80 <hr.bolouri@gmail.com>
2021-04-11 08:07:10 +0430

    add chunck support

---
+++ SHA256/sha256.nim
@@ -0,0 +1,230 @@
+import
+  sequtils, strutils,
+  algorithm, math,
+  os
+
+func `xor`*(a, b: seq[bool]): seq[bool] =
+  doAssert a.len == b.len, $(a, b)
+  for i in 0..<a.len:
+    result.add a[i] xor b[i]
+
+func `+`*(a, b: seq[bool]): seq[bool] =
+  doAssert a.len == b.len
+  var carry = 0
+  for i in countdown(a.len-1, 0):
+    let sum = carry + a[i].int + b[i].int
+    result.insert (sum mod 2).bool, 0
+    carry = sum div 2
+
+func `and`*(a, b: seq[bool]): seq[bool] =
+  doAssert a.len == b.len
+  for i in 0..<a.len:
+    result.add (a[i] == b[i]) and a[i]
+
+func `not`*(s: seq[bool]): seq[bool] =
+  s.mapIt not it
+
+
+func rightshifted*[T](s: seq[T]; num: Natural): seq[T] =
+  repeat(false, num) & s[0..<s.len-num]
+
+func groupEvery*[T](s: seq[T]; num: Natural): seq[seq[T]] =
+  for n in countup(0, s.len-1, num):
+    result.add s[n..<n+num]
+
+func toSeqBool*(s: string): seq[bool]{.inline.} =
+  s.mapIt it == '1'
+
+func toSeqBool*(s: openArray[int]): seq[bool]{.inline.} =
+  s.mapIt it == 1
+
+func toInt*(s: seq[bool]): int64 =
+  for i in 0..<s.len:
+    result += s[^(i+1)].int * (2^(i))
+
+const zeroList = [0, 0, 0, 0, 0, 0, 0, 0].toSeqBool
+
+func intToBinary*(num: int | int64; chunkBy = 8): seq[seq[bool]] {.inline.} =
+  var
+    numc = num
+    binaryRepr: seq[bool]
+
+  while numc != 0:
+    binaryRepr.insert bool(numc mod 2), 0
+    numc = numc div 2
+
+  let mustAdd = block:
+    let t = binaryRepr.len mod chunkBy
+    if t == 0: 0
+    else: chunkBy - t
+
+  binaryRepr.insert repeat(false, mustAdd), 0
+  binaryRepr.groupEvery(chunkBy)
+
+func strToBinarySeq*(word: string): seq[seq[bool]] {.inline.} =
+  word.mapIt it.ord.intToBinary[0]
+
+func add1toEnd(binarySeq: seq[seq[bool]]): seq[seq[bool]] {.inline.} =
+  binarySeq & [1, 0, 0, 0, 0, 0, 0, 0].toSeqBool
+
+func to512diviableMinus64*(binarySeq: seq[seq[bool]]): seq[seq[bool]] {.inline.} =
+  let remaining = block:
+    let 
+      t = (binarySeq.len * 8) mod 512
+      r = (512 - 64 - t)
+
+    if r < 0: r + 512
+    else: r
+
+
+  binarySeq & repeat(zeroList, remaining div 8)
+
+func `$`*(s: seq[bool]): string =
+  s.mapIt(if it: 1 else: 0).join
+
+func `$`*(s: seq[seq[bool]]): string =
+  s.mapIt($it)
+    .distribute(s.len div 2).mapIt(it.join " ")
+    .join "\n"
+
+const
+  hashes = [
+    0x6a09e667'i64,
+    0xbb67ae85,
+    0x3c6ef372,
+    0xa54ff53a,
+    0x510e527f,
+    0x9b05688c,
+    0x1f83d9ab,
+    0x5be0cd19,
+  ].mapIt it.intToBinary.concat
+  roundK = [
+    0x428a2f98'i64,
+    0x71374491,
+    0xb5c0fbcf,
+    0xe9b5dba5,
+    0x3956c25b,
+    0x59f111f1,
+    0x923f82a4,
+    0xab1c5ed5,
+    0xd807aa98,
+    0x12835b01,
+    0x243185be,
+    0x550c7dc3,
+    0x72be5d74,
+    0x80deb1fe,
+    0x9bdc06a7,
+    0xc19bf174,
+    0xe49b69c1,
+    0xefbe4786,
+    0x0fc19dc6,
+    0x240ca1cc,
+    0x2de92c6f,
+    0x4a7484aa,
+    0x5cb0a9dc,
+    0x76f988da,
+    0x983e5152,
+    0xa831c66d,
+    0xb00327c8,
+    0xbf597fc7,
+    0xc6e00bf3,
+    0xd5a79147,
+    0x06ca6351,
+    0x14292967,
+    0x27b70a85,
+    0x2e1b2138,
+    0x4d2c6dfc,
+    0x53380d13,
+    0x650a7354,
+    0x766a0abb,
+    0x81c2c92e,
+    0x92722c85,
+    0xa2bfe8a1,
+    0xa81a664b,
+    0xc24b8b70,
+    0xc76c51a3,
+    0xd192e819,
+    0xd6990624,
+    0xf40e3585,
+    0x106aa070,
+    0x19a4c116,
+    0x1e376c08,
+    0x2748774c,
+    0x34b0bcb5,
+    0x391c0cb3,
+    0x4ed8aa4a,
+    0x5b9cca4f,
+    0x682e6ff3,
+    0x748f82ee,
+    0x78a5636f,
+    0x84c87814,
+    0x8cc70208,
+    0x90befffa,
+    0xa4506ceb,
+    0xbef9a3f7,
+    0xc67178f2
+  ].mapIt it.intToBinary(8*4).concat
+
+func sha256*(s: string): string =
+  let t = (s.len * 8).intToBinary
+  var
+    table = concat([
+      s.strToBinarySeq.add1toEnd.to512diviableMinus64,
+      repeat(zeroList, 8 - t.len) & t])
+
+    hs = hashes
+
+  for chunck in table.groupEvery(8*8):
+    var words: seq[seq[bool]]
+    for i in countup(0, chunck.len-1, 4):
+      words.add concat(chunck[i..i+3])
+
+    doAssert words.len == 16
+
+    for i in 16..<64:
+      let
+        s0 =
+          (words[i-15].rotatedLeft -7) xor
+          (words[i-15].rotatedLeft -18) xor
+          (words[i-15].rightshifted 3)
+        s1 =
+          (words[i-2].rotatedLeft -17) xor
+          (words[i-2].rotatedLeft -19) xor
+          (words[i-2].rightshifted 10)
+
+      words.add s0 + s1 + words[i-7] + words[i-16]
+
+    var (a, b, c, d, e, f, g, h) = (
+      hs[0], hs[1], hs[2], hs[3], hs[4], hs[5], hs[6], hs[7])
+
+    for i in 0..<64:
+      let
+        s0 =
+          (a.rotatedLeft -2) xor
+          (a.rotatedLeft -13) xor
+          (a.rotatedLeft -22)
+
+        s1 =
+          (e.rotatedLeft -6) xor
+          (e.rotatedLeft -11) xor
+          (e.rotatedLeft -25)
+
+        ch = (e and f) xor ((not e) and g)
+        temp1 = h + s1 + ch + roundK[i] + words[i]
+
+        maj = (a and b) xor (a and c) xor (b and c)
+        temp2 = s0 + maj
+
+      (h, g, f, e, d, c, b, a) = (g, f, e, d + temp1, c, b, a, temp1 + temp2)
+
+    for (i, v) in [a, b, c, d, e, f, g, h].pairs:
+      hs[i] = v + hs[i]
+
+  hs.mapIt(it.toInt.toHex 8).join
+
+
+if isMainModule:
+  if paramCount() != 1:
+    echo "enter something to hash"
+  else:
+    echo sha256 paramStr 1
---
+++ SHA256/test.nim
@@ -0,0 +1,56 @@
+import sequtils, strutils, unittest
+import sha256
+
+suite "functionalities":
+  test "intToBinary":
+    check 001.intToBinary == @[[0, 0, 0, 0, 0, 0, 0, 1].toSeqBool]
+    check 088.intToBinary == @[[0, 1, 0, 1, 1, 0, 0, 0].toSeqBool]
+    check 127.intToBinary == @[[0, 1, 1, 1, 1, 1, 1, 1].toSeqBool]
+    check 280.intToBinary == @[
+      [0, 0, 0, 0, 0, 0, 0, 1].toSeqBool,
+      [0, 0, 0, 1, 1, 0, 0, 0].toSeqBool
+    ]
+
+  test "to512diviableMinus64":
+    check to512diviableMinus64(@[
+      [0, 0, 0, 0, 0, 0, 0, 1].toSeqBool
+    ]).len == 56
+
+    check to512diviableMinus64(
+      [0, 0, 0, 0, 0, 0, 0, 1].toSeqBool.repeat(56)
+    ).len == 56
+
+    check to512diviableMinus64(
+      [0, 0, 0, 0, 0, 0, 0, 1].toSeqBool.repeat(64)
+    ).len == 120
+
+  test "rightHsifted":
+    check "01101111001000000111011101101111".toSeqBool.rightshifted(3) ==
+        "00001101111001000000111011101101".toSeqBool
+
+  test "groupEvery":
+    check (@[0,1 ,2,3 ,4,5, 6,7, 8,9].groupEvery 2).len == 5
+    check (@[0,1 ,2,3 ,4,5, 6,7, 8,9].groupEvery 2) == @[@[0,1] ,@[2,3] ,@[4,5], @[6,7], @[8,9]]
+    
+
+suite "binary operations":
+  test "+":
+    check (
+          "01101000011001010110110001101100".toSeqBool +
+          "11001110111000011001010111001011".toSeqBool
+      ) == "00110111010001110000001000110111".toSeqBool
+
+  test "not":
+    check "11101001".toSeqBool.not == "00010110".toSeqBool
+
+suite "sha256":
+  test "hello world":
+    check "hello world".sha256 == "B94D27B9934D3E08A52E52D7DA7DABFAC484EFE37A5380EE9088F7ACE2EFCDE9"
+
+  test "\\n":
+    check "\n".sha256 == "01BA4719C80B6FE911B091A7C05124B64EEECE964E09C058EF8F9805DACA546B"
+
+
+  test "sample.txt":
+    check (sha256 readfile "sample.txt").tolower == "fcbb9be766b9cfdd0c1697fad8ca001879b48acbbcfd5460f71d8cef82cf0caa".toLower
+    
