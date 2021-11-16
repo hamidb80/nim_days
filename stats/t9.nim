c1fbd0ab0a236b3310b99d73266bf73ac54605c1
hamidb80 <hr.bolouri@gmail.com>
2021-11-16 22:33:30 +0330

    t9

---
+++ stats/t9.nim
@@ -0,0 +1,14 @@
+import sugar
+import formula
+
+
+proc drawFuncWith(probability: float)=
+  let points = collect newseq:
+    for k in 0..100:
+      (k.float, poisson(100, k, probability))
+
+  showInDiagram points
+
+
+for p in [0.3, 0.5, 0.9]:
+  drawFuncWith p
