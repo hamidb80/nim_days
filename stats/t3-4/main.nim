e58c9426cdf5f80e5f91a9f6ee9a003d85fda9dc
hamidb80 <hr.bolouri@gmail.com>
2021-11-29 14:28:36 +0330

    ad t3 & t4

---
+++ stats/t3-4/main.nim
@@ -0,0 +1,42 @@
+import sequtils, random
+
+const
+  Healthy = true
+  Broken = false
+  Total = 1_000_000
+
+proc p1(): int =
+  var
+    lamps = Broken.repeat(20) & Healthy.repeat(80)
+    desired = 0
+
+  for _ in 1..Total:
+    lamps.shuffle()
+
+    if lamps[0..<12].countIt(it == Broken) == 3:
+      desired += 1
+
+  desired
+
+
+proc p2(): int =
+  var desired = 0
+  for _ in 1..Total:
+    let broken =
+      (1..12).toseq()
+      .mapIt(rand(1..100) <= 20)
+      .countIt(it == true)
+
+    if broken == 3:
+      desired += 1
+
+  desired
+
+
+for _ in 1..10:
+  echo p1() / Total
+
+echo "-----------------"
+
+for _ in 1..10:
+  echo p2() / Total
