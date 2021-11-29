87f579c7600f364fd78220b5eafa7d8259a01988
hamidb80 <hr.bolouri@gmail.com>
2021-11-29 16:10:21 +0330

    make it dynamic

--- stats/t3-4/main.nim
+++ stats/t3-4/main.nim
@@ -4,59 +4,54 @@ const
   Healthy = true
   Broken = false
   Total = 1_000_000
+  Select = 12
+  Desired = 3
+  Repeat = 3
+
+func toseq(n: int): seq[int] = (1..n).toseq
 
 proc p6(): int =
-  var
-    lamps = Broken.repeat(20) & Healthy.repeat(80)
-    desired = 0
+  var lamps = Broken.repeat(20) & Healthy.repeat(80)
 
   for _ in 1..Total:
     lamps.shuffle()
 
-    if lamps[0..<12].countIt(it == Broken) == 3:
-      desired += 1
-
-  desired
+    if lamps[0..<Select].countIt(it == Broken) == Desired:
+      result += 1
 
 
 proc p5_1(): int =
-  var desired = 0
   for _ in 1..Total:
     let broken =
-      (1..12)
+      Select
       .toseq()
       .mapIt(rand 1..100)
       .filterIt(it <= 20)
       .len
 
-    if broken == 3:
-      desired += 1
-
-  desired
+    if broken == Desired:
+      result += 1
 
 
 proc p5_2(): int =
-  var desired = 0
   for _ in 1..Total:
-    let broken = 
+    let broken =
       (1..100)
       .toseq()
-      .mapIt((rand 1..100) <= 20)[0..<12]
+      .mapIt((rand 1..100) <= 20)[0..<Select]
       .countIt(it == true)
 
-    if broken == 3:
-      desired += 1
-
-  desired
+    if broken == Desired:
+      result += 1
 
 # ------------------------------------------------
 
 template calc(title, fn): untyped =
   echo "<<< ", title, " >>>"
 
-  for _ in 1..10:
+  for _ in 1..Repeat:
     echo fn() / Total
-  
+
 
 calc "p5_1", p5_1
 calc "p5_2", p5_2
